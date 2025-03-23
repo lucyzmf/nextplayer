ca6e43398ac55c364a9bd3004a7b6923b6f8131d
lucyzmf <lucy.zmf@gmail.com>
2025-03-23 11:23:24 +0100

    fix: remove problematci import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,6 @@ dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:ui"))
     implementation(project(":core:serial"))
-    implementation(project(":core"))
     implementation(project(":feature:videopicker"))
     implementation(project(":feature:player"))
     implementation(project(":feature:settings"))
